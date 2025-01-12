bcc88db6a5e4d605aa5af12b81e59458db23d1ce
ShobhitChaturvedi <113877844+ShobhitChaturvedi@users.noreply.github.com>
2025-01-12 23:21:00 +0530

    Time: 0 ms (100%), Space: 41.6 MB (58.89%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int[] arr=new int[26];
+        for(int i=0;i<s.length();i++){
+            if(arr[s.charAt(i)-'a']==1){
+                return s.charAt(i);
+            }else{
+                arr[s.charAt(i)-'a']=1;
+            }
+        }
+        return ' ';
+    }
+}
