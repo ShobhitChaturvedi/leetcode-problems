abc63c6f3d8661f1294c4c15fc4a1a8c5c4704a6
ShobhitChaturvedi <113877844+ShobhitChaturvedi@users.noreply.github.com>
2025-06-19 17:14:16 +0530

    Time: 1 ms (10.07%), Space: 45.4 MB (97.85%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -3,12 +3,10 @@ class Solution {
         int t=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
-                return t;
+                return i;
             }
             t++;
-            if(t==nums.length){
-                return -1;
-            }
-        }return t;
+            
+        }return -1;
     }
 }
